c4514919c71ea88e393fe2c608357457badd6f0f
adiko <dikodam@googlemail.com>
2018-12-01 22:40:43 +0100

    Initial commit

---
+++ src/main/kotlin/AbstractDay.kt
@@ -0,0 +1,23 @@
+package de.dikodam.adventofkotlin
+
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+import kotlin.streams.toList
+
+abstract class AbstractDay {
+    abstract fun task1()
+    abstract fun task2()
+
+    fun readInputLines(): List<String> {
+        val dayName = javaClass.simpleName
+        val inputFile = File(javaClass.getResource("/$dayName").toURI())
+        return BufferedReader(FileReader(inputFile))
+                .run { lines().toList() }
+    }
+}
+
+fun AbstractDay.runTasks() {
+    this.task1()
+    this.task2()
+}
