18b39da6b6c15885fc4dcc8656d14741c1354465
adiko <dikodam@googlemail.com>
2018-12-05 15:29:03 +0100

    day 5 task 1 + 2

---
+++ src/main/kotlin/Day05.kt
@@ -0,0 +1,46 @@
+package de.dikodam.adventofkotlin
+
+fun main(args: Array<String>) {
+    Day05().runTasks()
+}
+
+internal fun reactPolymerWithNextChar(polymer: String, nextChar: Char): String {
+    val eliminatedString = { polymer.substring(0..(polymer.length - 2)) }
+    return when {
+        polymer.isEmpty() -> nextChar.toString()
+        polymer.last().isSameCharDifferentCase(nextChar) -> eliminatedString()
+        else -> polymer + nextChar
+    }
+}
+
+internal fun Char.isSameCharDifferentCase(otherChar: Char): Boolean {
+    return this.equals(otherChar, ignoreCase = true) &&
+            !this.equals(otherChar, ignoreCase = false)
+}
+
+internal fun String.reactPolymer() = this.asSequence()
+        .fold("")
+        { leftString, nextChar -> reactPolymerWithNextChar(leftString, nextChar) }
+
+
+class Day05 : AbstractDay() {
+
+    private val input = inputLines[0]
+
+    internal fun String.filterOutBothCases(badChar: Char): String {
+        return this.filter { stringChar -> stringChar != badChar.toLowerCase() && stringChar != badChar.toUpperCase() }
+    }
+
+    override fun task1() {
+        val reactedPolymerLength = input.reactPolymer().count()
+        println("TASK 1: $reactedPolymerLength")
+    }
+
+    override fun task2() {
+        val chars = 'a'..'z'
+        val filteredPolymers = chars.map { char -> input.filterOutBothCases(char) }
+        val shortestFilteredPolymer = filteredPolymers.map { it.reactPolymer().count() }.min()
+
+        println("TASK 2: $shortestFilteredPolymer")
+    }
+}
---
+++ src/test/kotlin/Day05Test.kt
@@ -0,0 +1,39 @@
+import de.dikodam.adventofkotlin.reactPolymerWithNextChar
+import de.dikodam.adventofkotlin.isSameCharDifferentCase
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+internal class Day05Test {
+
+    @Test
+    fun `eliminates char at end of string if equal with parameter but different case`() {
+        assertThat(reactPolymerWithNextChar("aC", 'c'))
+                .isEqualTo("a")
+    }
+
+    @ParameterizedTest
+    @MethodSource("charComparisonProvider")
+    fun `same chars with different case`(data: CharComparison) {
+        assertThat(data.firstChar.isSameCharDifferentCase(data.secondChar)).isEqualTo(data.expectedBoolean)
+    }
+
+    fun charComparisonProvider() = Stream.of(
+            CharComparison('A', 'a', true),
+            CharComparison('b', 'B', true),
+            CharComparison('c', 'c', false),
+            CharComparison('C', 'C', false),
+            CharComparison('d', 'e', false),
+            CharComparison('f', 'G', false)
+    )
+
+    data class CharComparison(
+            val firstChar: Char,
+            val secondChar: Char,
+            val expectedBoolean: Boolean
+    )
+}
