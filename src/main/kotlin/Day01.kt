0bf239effc30ffd109026d06879260834b395eb1
adiko <dikodam@googlemail.com>
2018-12-07 13:58:46 +0100

    day 07 started

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -6,30 +6,29 @@ fun main(args: Array<String>) {
 
 class Day01 : AbstractDay() {
 
+    private val frequencyChanges: List<Int> = inputLines.map { Integer.parseInt(it) }
+
     override fun task1() {
-        val sumOfFrequencies = inputLines
-                .map { Integer.parseInt(it) }
-                .sum()
+        val sumOfFrequencies = frequencyChanges.sum()
         println("TASK 1: The sum of frequencies is $sumOfFrequencies")
     }
 
     override fun task2() {
-        val frequencyChanges = inputLines.map { Integer.parseInt(it) }
-
-        var i = 0
-        val nextFrequencyChange: () -> Int = {
-            val change = frequencyChanges[i % frequencyChanges.size]
-            i++
-            change
-        }
+        val getNextFrequencyChange: () -> Int = {
+            var i = 0
+            {
+                val change = frequencyChanges[i % frequencyChanges.size]
+                i++
+                change
+            }
+        }()
 
         val frequencyBuffer = HashSet<Int>()
-        val isFrequencyUnique: (Int) -> Boolean = { frequency -> frequencyBuffer.add(frequency) }
+        fun Int.isUniqueSoFar(): Boolean = frequencyBuffer.add(this)
 
         var currentFrequency = 0
-
-        while (isFrequencyUnique(currentFrequency)) {
-            currentFrequency += nextFrequencyChange()
+        while (currentFrequency.isUniqueSoFar()) {
+            currentFrequency += getNextFrequencyChange()
         }
 
         println("TASK 2: first duplicate frequency is $currentFrequency")
---
+++ src/main/kotlin/Day07.kt
@@ -0,0 +1,31 @@
+package de.dikodam.adventofkotlin
+
+fun main(args: Array<String>) {
+    Day07().runTasks()
+}
+
+class Day07 : AbstractDay() {
+    override fun task1() {
+        val temporalDependencies = inputLines.map { it.parseTemporalDependency() }
+
+        val result = "implementing"
+        println("TASK 1: $result")
+    }
+
+    override fun task2() {
+        val result = "not implemented yet"
+        println("TASK 2: $result")
+    }
+}
+
+// Step F must be finished before step E can begin.
+internal fun String.parseTemporalDependency(): Day07TemporalDependency {
+    val independentTask = this[5]
+    val dependentTask = this[36]
+    return Day07TemporalDependency(independentTask, dependentTask)
+}
+
+data class Day07TemporalDependency(
+        val independentTask: Char,
+        val dependentTask: Char
+)
