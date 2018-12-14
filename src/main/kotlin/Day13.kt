5482654877a3a5928377de6ecc47c647af85cba3
adiko <dikodam@googlemail.com>
2018-12-14 10:31:28 +0100

    day 13 class boilerplate, inputfile moved to correct dir

---
+++ src/main/kotlin/Day13.kt
@@ -0,0 +1,17 @@
+package de.dikodam.adventofkotlin
+
+fun main(args: Array<String>) {
+    Day13().runTasks()
+}
+
+class Day13 : AbstractDay() {
+    override fun task1() {
+        val result = "not implemented yet"
+        println("TASK 1: $result")
+    }
+
+    override fun task2() {
+        val result = "not implemented yet"
+        println("TASK 2: $result")
+    }
+}
