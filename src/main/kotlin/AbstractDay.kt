4f9edf905139aa223a4b0a569b1f47e6083768e3
adiko <dikodam@googlemail.com>
2019-09-12 11:07:31 +0200

    Day 04 finished

--- src/main/kotlin/AbstractDay.kt
+++ src/main/kotlin/AbstractDay.kt
@@ -6,14 +6,12 @@ abstract class AbstractDay {
     abstract fun task1()
     abstract fun task2()
 
-    protected val inputLines = readInputLines()
+    protected val inputLines by lazy { readInputLines() }
 
     private fun readInputLines(): List<String> {
         val dayName = javaClass.simpleName
         val inputFile = File(javaClass.getResource("/$dayName").toURI())
-        return inputFile.useLines { it.toList()}
-        // return BufferedReader(FileReader(inputFile))
-        //         .use { reader -> reader.lines().toList() }
+        return inputFile.useLines { it.toList() }
     }
 
     fun runTasks() {
--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -9,60 +9,50 @@ fun main() {
     Day04().runTasks()
 }
 
-enum class GuardState { ASLEEP, AWAKE }
-data class GuardEvent(val guardID: Int, val time: LocalDateTime, val event: GuardState)
-data class Accumulator(val currentGuardID: Int, val eventsByGuard: MutableMap<Int, List<GuardEvent>>)
+private enum class GuardState { ASLEEP, AWAKE }
+private data class GuardEvent(val guardID: Int, val time: LocalDateTime, val event: GuardState)
+private data class Accumulator(val currentGuardID: Int, val eventsByGuard: MutableMap<Int, List<GuardEvent>>)
+private typealias DayMinutes = Pair<Int, IntRange>
 
 class Day04 : AbstractDay() {
-    // line :
-    //      DATE EVENT
-    // DATE:
-    //      [year-month-day hour:minute]
-    // EVENT:
-    //      Guard ID begins shift
-    //      falls asleep
-    //      wakes up
-    override fun task1() {
+
+    private val sleepingSchedule by lazy { parseInput() }
+
+    private fun parseInput(): Map<Int, List<DayMinutes>> {
         val events = inputLines
             .map { splitToLocalDateTimeAndEvent(it) }
             .sortedBy { (dateTime, _) -> dateTime }
 
-        // events.forEach { println(it) }
-
-        val (_, eventsByGuard) = events.fold(initial = Accumulator(-1, HashMap()))
-        { accumulator, next ->
-            val (currentGuardID, eventsByGuard) = accumulator
-            val (eventDatetime, eventString) = next
-
-            if (eventString.isGuardID()) {
-                Accumulator(extractGuardID(eventString), eventsByGuard)
-            } else {
-                val guardEvent = listOf(GuardEvent(currentGuardID, eventDatetime, eventString.toGuardState()))
-                eventsByGuard.merge(currentGuardID, guardEvent)
-                { list1, list2 ->
-                    sequenceOf(list1, list2).flatten().toList()
+        val eventsByGuard = events
+            .fold(initial = Accumulator(-1, HashMap()))
+            { accumulator, nextEvent ->
+                val (currentGuardID, eventsByGuard) = accumulator
+                val (eventDatetime, eventString) = nextEvent
+                if (eventString.isGuardID()) {
+                    val guardID = extractGuardID(eventString)
+                    Accumulator(guardID, eventsByGuard)
+                } else {
+                    val guardEvent = listOf(GuardEvent(currentGuardID, eventDatetime, eventString.toGuardState()))
+                    eventsByGuard.merge(currentGuardID, guardEvent) { list1, list2 ->
+                        sequenceOf(list1, list2).flatten().toList()
+                    }
+                    Accumulator(currentGuardID, eventsByGuard)
                 }
-                Accumulator(currentGuardID, eventsByGuard)
             }
-        }
+            .eventsByGuard
 
-        eventsByGuard.toSortedMap()
+        return eventsByGuard
+            .toSortedMap()
             .mapValues { (_, events) ->
-                events.zipWithNext()
+                events.windowed(size = 2, step = 2) { it[0] to it[1] }
                     .map { it.minutesAsleepOnDay() }
             }
-            .forEach { (k, v) -> v.forEach { println("guardID: $k, $it") } }
-
-
-        val result = "implementing..."
-        println("TASK 1: $result")
     }
 
-    private fun Pair<GuardEvent, GuardEvent>.minutesAsleepOnDay(): Pair<Int, IntRange> {
+    private fun Pair<GuardEvent, GuardEvent>.minutesAsleepOnDay(): DayMinutes {
         val (start, end) = this
         val minutes = start.time.minute until end.time.minute
         return start.time.dayOfMonth to minutes
-
     }
 
     private fun String.toGuardState() =
@@ -79,7 +69,7 @@ class Day04 : AbstractDay() {
         eventString.split(" ")[1].substring(startIndex = 1).toInt()
 
     // "[1518-07-03 23:58] EVENTSTRING"
-    public fun splitToLocalDateTimeAndEvent(inputLine: String): Pair<LocalDateTime, String> {
+    private fun splitToLocalDateTimeAndEvent(inputLine: String): Pair<LocalDateTime, String> {
         val year = inputLine.substring(1..4).toInt()
         val month = Month.of(inputLine.substring(6..7).toInt())
         val day = inputLine.substring(9..10).toInt()
@@ -90,8 +80,56 @@ class Day04 : AbstractDay() {
         return Pair(dateTime, eventString)
     }
 
-    override fun task2() {
-        val result = "not implemented yet"
+    private fun countSleepingMinutes(dayMinutesAsleep: List<DayMinutes>): Map<Int, Int> {
+        return dayMinutesAsleep
+            .asSequence()
+            .flatMap { (_, minutes) -> minutes.asSequence() }
+            .groupingBy { it }
+            .eachCount()
+    }
+
+    // line :
+    //      DATE EVENT
+    // DATE:
+    //      [year-month-day hour:minute]
+    // EVENT:
+    //      Guard ID begins shift
+    //      falls asleep
+    //      wakes up
+    override fun task1() {
+        val guardSleepingMost = sleepingSchedule.maxBy { (_, dayMinutes) ->
+            dayMinutes
+                .map { (_, minutes) -> minutes.count() }
+                .sum()
+        }!! // there is one guard for sure, so we enforce non-nullness
+
+        val (guardID: Int, dayMinutesAsleep: List<DayMinutes>) = guardSleepingMost
+        val minuteCounts = countSleepingMinutes(dayMinutesAsleep)
+        val (minute, _) = minuteCounts
+            .maxBy { (_, count) -> count }!! // there is at least one minute, so we enforce non-nullness
+
+        val result = guardID * minute
         println("TASK 1: $result")
+    }
+
+    override fun task2() {
+        // Of all guards, which guard is most frequently asleep on the same minute?
+        val (guardID, minuteCount) =
+            sleepingSchedule
+                .mapValues { (_, dayMinutes) ->
+                    countSleepingMinutes(dayMinutes)
+                }
+                .mapValues { (_, minuteCounts) ->
+                    minuteCounts.maxBy { entry -> entry.value }!!
+                }
+                .maxBy { (_, minuteCount) ->
+                    val (_, count) = minuteCount
+                    count
+                }!!
+
+        val (minute, _) = minuteCount
+        val result = guardID * minute
+        // What is the ID of the guard you chose multiplied by the minute you chose?
+        println("TASK 2: $result")
     }
 }
