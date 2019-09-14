c93ba065eb8e5cfb02635d309ae911450b4e5ce5
adiko <dikodam@googlemail.com>
2019-09-14 17:03:29 +0200

    day07 first task

--- src/main/kotlin/Day07.kt
+++ src/main/kotlin/Day07.kt
@@ -6,10 +6,38 @@ fun main() {
 
 class Day07 : AbstractDay() {
     override fun task1() {
-        val temporalDependencies = inputLines.map { it.parseTemporalDependency() }
+        var temporalDependencies = inputLines.map { it.parseTemporalDependency() }
+        val allTasks = temporalDependencies
+            .asSequence()
+            .flatMap { sequenceOf(it.dependentTask, it.independentTask) }
+            .toSet()
 
+        var path = ""
+        while (temporalDependencies.isNotEmpty()) {
+            val (root, newList) = extractRoot(temporalDependencies)
+            println("extracted root: $root")
+            path += root
+            temporalDependencies = newList
+        }
 
-        val result = "implementing"
+        // hack for the last one
+        path += allTasks.filter { !path.contains(it) }[0]
+
+        // the source is the vortex that has no incoming edge.
+        // if there are more possible sources, the source is the vortex which comes first alphabetically.
+        // keep extracting sources until all vertices are extracted.
+
+        // IDEA 1:
+        // build directed graph of dependencies
+        // on each step, extract source.
+
+        // IDEA 2:
+        // gather all dependency pairs.
+        // a viable source in a vortex that isnt listed on the right hand side (receiving/incoming end)
+        // identify all viable sources, pick the one that comes first alphabetically.
+        // upon extraction, delete all dependencies with the extracted source on the left side.
+
+        val result = path
         println("TASK 1: $result")
     }
 
@@ -20,13 +48,21 @@ class Day07 : AbstractDay() {
 }
 
 // Step F must be finished before step E can begin.
-internal fun String.parseTemporalDependency(): Day07TemporalDependency {
+private fun String.parseTemporalDependency(): Day07TemporalDependency {
     val independentTask = this[5]
     val dependentTask = this[36]
     return Day07TemporalDependency(independentTask, dependentTask)
 }
 
-data class Day07TemporalDependency(
+private fun extractRoot(dependencies: List<Day07TemporalDependency>): Pair<Char, List<Day07TemporalDependency>> {
+    val dependentTasks = dependencies.map { it.dependentTask }.toSet()
+    val independentTasks = dependencies.map { it.independentTask }.toSet()
+    val root = independentTasks.filter { !dependentTasks.contains(it) }.sorted()[0]
+    val newDependencies = dependencies.filter { it.independentTask != root }
+    return Pair(root, newDependencies)
+}
+
+private data class Day07TemporalDependency(
     val independentTask: Char,
     val dependentTask: Char
 )
