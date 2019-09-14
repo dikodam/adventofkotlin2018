9339134e01578f5f08a8349973f98467b5b8856a
adiko <dikodam@googlemail.com>
2019-09-14 17:07:19 +0200

    day06 boilerplate

---
+++ src/main/kotlin/Day06.kt
@@ -0,0 +1,22 @@
+package de.dikodam.adventofkotlin
+
+fun main() {
+    Day06().runTasks()
+}
+
+class Day06 : AbstractDay() {
+
+    override fun task1() {
+
+
+        val result = "not implemented yet"
+        println("TASK 1: $result")
+    }
+
+    override fun task2() {
+
+
+        val result = "not implemented yet"
+        println("TASK 2: $result")
+    }
+}
